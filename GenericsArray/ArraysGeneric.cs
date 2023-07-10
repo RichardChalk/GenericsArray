73a75c78cca0621bf867f89739c3c323c5512f2f
Richard Chalk <barmybrit@gmail.com>
2023-07-10 12:29:30 +0200

    Add project files.

---
+++ GenericsArray/ArraysGeneric.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsArray
+{
+    public class ArraysGeneric
+    {
+        // Generics
+        public T[] CreateArrayGeneric<T>(T item1, T item2)
+        {
+            return new T[]
+            {
+                item1,
+                item2
+            };
+        }
+
+        // Method overloading
+        public int[] CreateArray(int item1, int item2) 
+        { 
+            return new int[]
+            {
+                item1,
+                item2
+            }; 
+        }
+
+        // Method overloading
+        public double[] CreateArray(double item1, double item2)
+        {
+            return new double[]
+            {
+                item1,
+                item2
+            };
+        }
+
+        // Method overloading
+        public string[] CreateArray(string item1, string item2)
+        {
+            return new string[]
+            {
+                item1,
+                item2
+            };
+        }
+    }
+}
---
+++ GenericsArray/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace GenericsArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ///////////////////////////////////////////////////////
+            Console.WriteLine("Generic Method");
+            Console.WriteLine("==============");
+            
+            // Int
+            var intArr = new ArraysGeneric().CreateArrayGeneric(45, 98);
+            foreach (var num in intArr)
+                Console.WriteLine(num);
+            // Double
+            var doubleArr = new ArraysGeneric().CreateArrayGeneric(45.3, 98.7);
+            foreach (var num in doubleArr)
+                Console.WriteLine(num);
+            // String
+            var stringArr = new ArraysGeneric().CreateArrayGeneric("Richard", "Stefan");
+            foreach (var name in stringArr)
+                Console.WriteLine(name);
+
+            // ///////////////////////////////////////////////////////
+            Console.WriteLine("\nMethod Overloading");
+            Console.WriteLine("==================");
+
+            // Int Generic
+            var intArrGen = new ArraysGeneric().CreateArray(45, 98);
+            foreach (var num in intArr)
+                Console.WriteLine(num);
+            // Double
+            var doubleArrGen = new ArraysGeneric().CreateArray(45.3, 98.7);
+            foreach (var num in doubleArr)
+                Console.WriteLine(num);
+            // String
+            var stringArrGen = new ArraysGeneric().CreateArray("Richard", "Stefan");
+            foreach (var name in stringArr)
+                Console.WriteLine(name);
+        }
+    }
+}
